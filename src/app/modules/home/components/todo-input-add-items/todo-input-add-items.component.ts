463d53f67976023688cdb3c10f7c7ccd442ab918
Kairuus <kingkairuus@gmail.com>
2023-05-09 10:59:09 -0300

    Project

---
+++ src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-input-add-items',
+  templateUrl: './todo-input-add-items.component.html',
+  styleUrls: ['./todo-input-add-items.component.css']
+})
+export class TodoInputAddItemsComponent {
+
+  @Output() public  emitItemTaskList = new EventEmitter();
+
+  public addItemTaskList = ''
+
+  public submitItemTaskList(){
+
+    this.addItemTaskList = this.addItemTaskList.trim()
+
+    if(this.addItemTaskList){
+      this.emitItemTaskList.emit(this.addItemTaskList)
+      this.addItemTaskList =""
+    }
+  }
+}
---
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -0,0 +1,48 @@
+import { TaskList } from './../../model/task-list';
+import { Component, DoCheck } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements DoCheck{
+  ngDoCheck() {
+    this.setLocalStorage()
+  }
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem('list') || '[]')
+
+  public setEmitTaskList(event: string){
+    this.taskList.push({task: event, checked:false})
+  }
+
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1)
+  }
+
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Você deseja realmente deletar todos os itens?")
+    if(confirm){
+      this.taskList = []
+    }
+  }
+
+  public validationInput(event: string, index: number){
+
+    if(!event.length){
+      const confirm = window.confirm('Task está vazia, deseja deletar?')
+
+      if(confirm){
+        this.deleteItemTaskList(index)
+      }
+    }
+  }
+
+  public setLocalStorage(){
+    if(this.taskList){
+      this.taskList.sort( (first, last) => Number(first.checked) - Number(last.checked))
+      localStorage.setItem('list', JSON.stringify(this.taskList))
+    }
+  }
+
+}
